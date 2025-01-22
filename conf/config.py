9b1c52a52293c4b0be8d8a94dcc711d93a179200
zhanghuan <13129933178@163.com>
2025-01-22 10:47:52 +0800

    modify config parser

--- conf/config.py
+++ conf/config.py
@@ -1,19 +1,23 @@
-import configparser
+# import configparser
+import ConfigParser
 from UserDict import UserDict
 
 # conf = configparser.ConfigParser()
 # conf.read('config.ini')
 
 class Conf(object):
-	def __init__(self, confName, confSection):
-		self.__dict__['confName'] = confName
-		self.__dict__['confSection'] = confSection
+	def __init__(self, sectionName, sectionInfoList):
+		self.__dict__['sectionName'] = sectionName
+		# self.__dict__['confSection'] = confSection
+		for confName, confVal in sectionInfoList:
+			self.__dict__[confName] = confVal
 
 	def __getattr__(self, name):
-		if name not in self.confSection:
-			raise Exception("not find conf '{0}.{1}' in config.ini".format(self.confName, name))
+		name = name.lower()
+		if name not in self.__dict__:
+			raise Exception("not find conf '{0}.{1}' in config.ini".format(self.sectionName, name))
 
-		return self.confSection[name]
+		return self.__dict__[name]
 
 	def __setattr__(self, name, value):
 		raise Exception("Disable configuration changes")
@@ -22,11 +26,13 @@ class AllConf(UserDict):
 	def __init__(self, confPath):
 		# super(AllConf, self).__init__()
 		UserDict.__init__(self)
-		self.conf = configparser.ConfigParser()
+		self.conf = ConfigParser.ConfigParser()
 		self.conf.read(confPath)
 
-		for confName, confVal in self.conf.iteritems():
-			self[confName] = Conf(confName, confVal)
+		# for confName, confVal in self.conf.iteritems():
+		# 	self[confName] = Conf(confName, confVal)
+		for sectionName in self.conf.sections():
+			self[sectionName] = Conf(sectionName, self.conf.items(sectionName))
 
 	def __getattr__(self, name):
 		if name not in self:
